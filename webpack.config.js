ac912c239b937757db33b534737f75a85dd91cf4
Meshach Mitchell <rain@mawu.home>
2018-12-07 14:42:15 -0500

    fix webpack public path for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     path:       path.join( __dirname, 'js/' ),
-    publicPath: path.join( __dirname, 'js/' ),
+    publicPath: '/moonraker-theme/js/',
     filename:   '[name].js'
   },
   plugins: [
