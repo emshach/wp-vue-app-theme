add80db5456f489daf85def5b87fcfd4ebe4b964
Meshach Mitchell <rain@mawu.home>
2018-12-12 14:14:46 -0500

    finish move to /style.css

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
           href="<?php echo $template_dir; ?>/css/dist/bootstrap-grid.css" />
     <link rel="stylesheet"
           href="<?php echo $template_dir; ?>/css/dist/bootstrap-reboot.css" />
-    <link rel="stylesheet" href="<?php echo $template_dir; ?>/css/dist/style.css" />
+    <link rel="stylesheet" href="<?php echo $template_dir; ?>/style.css" />
     <?php wp_head(); ?>
   </head>
   <body>
