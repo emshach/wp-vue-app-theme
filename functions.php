728da636b42349c5641fe1d2d433a7e4224540c3
Meshach Mitchell <rain@mawu.home>
2018-12-13 12:05:44 -0500

    trying to make collection posts act right

--- functions.php
+++ functions.php
@@ -186,6 +186,7 @@ function mrk_rest_add_promo_reel( $data ) {
         return $data;
     $posts = get_posts(
         [
+            'showpoosts' => -1,
             'nopaaging' => true,
             'tax_query' => [
                 [
