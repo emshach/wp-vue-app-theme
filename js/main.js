68ecfd99cd2f9214f1acbf543498ce4702110cfb
Meshach Mitchell <rain@mawu.home>
2018-12-18 20:36:42 -0500

    render episodes list, first try

--- js/main.js
+++ js/main.js
@@ -1,229 +1,101 @@
-import 'babel-polyfill';
-import 'es6-promise/auto';
-import axios from 'axios';
-import Vue from 'vue';
-import Vuex from 'vuex';
-import VueRouter from 'vue-router';
-import BootstrapVue from 'bootstrap-vue';
-import { FlowerSpinner } from 'epic-spinners';
-import VueCarousel from 'vue-carousel';
-import VueLazyload from 'vue-lazyload-img';
-import VueSweetalert2 from 'vue-sweetalert2';
-import store from './lib/store';
-import wpapix from './lib/wpapi';
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/wp-content/themes/moonraker-theme/js/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/*!************************!*\
+  !*** multi git commit ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
 
-// styles
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-// import 'swiper/dist/css/swiper.css';
+!(function webpackMissingModule() { var e = new Error("Cannot find module 'git'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+!(function webpackMissingModule() { var e = new Error("Cannot find module 'commit'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
 
-//partial components
-const TheLoop     = () => import( './components/the-loop'     );
-const Sidebar     = () => import( './components/sidebar'      );
-const WpFooter    = () => import( './components/wp-footer'    );
-const NoPost      = () => import( './components/nopost'       );
-const WpHeader    = () => import( './components/wp-header'    );
-const SearchForm  = () => import( './components/search-form'  );
-const CommentForm = () => import( './components/comment-form' );
-const Comments    = () => import( './components/comments'     );
-const Carousel    = () => import( './components/mrk-carousel' );
-const ContentList = () => import( './components/content-list' );
-const Filmstrip   = () => import( './components/filmstrip'    );
-const NavMenu     = () => import( './components/nav-menu'     );
-// const             = () => import( './components/'             );
 
-// const components with routes
-const PageNotFound   = () => import( './components/page-not-found'  );
-const Home           = () => import( './components/home'            );
-const Login          = () => import( './components/login'           );
-const About          = () => import( './components/about'           );
-const Contact        = () => import( './components/contact'         );
-const Single         = () => import( './components/single'          );
-const Page           = () => import( './components/page'            );
-const Post           = () => import( './components/post'            );
-const Archive        = () => import( './components/archive'         );
-const Search         = () => import( './components/search'          );
-const Program        = () => import( './components/program'         );
-const Release        = () => import( './components/release'         );
-const PreviewProgram = () => import( './components/preview-program' );
-const PreviewRelease = () => import( './components/preview-release' );
-const Members        = () => import( './components/members'         );
-const Upload         = () => import( './components/upload'          );
-const Shop           = () => import( './components/shop'            );
-
-window.Vue = Vue;
-
-// directives
-Vue.config.devtools = true;
-Vue.use( Vuex );
-Vue.use( VueRouter );
-Vue.use( BootstrapVue );
-Vue.use( VueCarousel );
-Vue.use( VueLazyload, { fade: true });
-Vue.use( VueSweetalert2 );
-
-// init components
-Vue.component( 'the-loop',       TheLoop       );
-Vue.component( 'sidebar',        Sidebar       );
-Vue.component( 'wp-footer',      WpFooter      );
-Vue.component( 'nopost',         NoPost        );
-Vue.component( 'wp-header',      WpHeader      );
-Vue.component( 'search-form',    SearchForm    );
-Vue.component( 'comment-form',   CommentForm   );
-Vue.component( 'comments',       Comments      );
-Vue.component( 'flower-spinner', FlowerSpinner );
-Vue.component( 'mrk-carousel',   Carousel      );
-Vue.component( 'content-list',   ContentList   );
-Vue.component( 'filmstrip',      Filmstrip     );
-Vue.component( 'nav-menu',       NavMenu       );
-// Vue.component( '',                             );
-
-//components with routes
-const VuePageNotFound   = Vue.component( 'page-not-found',  PageNotFound   );
-const VueHome           = Vue.component( 'home',            Home           );
-const VueLogin          = Vue.component( 'login',           Login          );
-const VueAbout          = Vue.component( 'about',           About          );
-const VueContact        = Vue.component( 'contact',         Contact        );
-const VueSingle         = Vue.component( 'single',          Single         );
-const VuePage           = Vue.component( 'page',            Page           );
-const VuePost           = Vue.component( 'post',            Post           );
-const VueArchive        = Vue.component( 'archive',         Archive        );
-const VueSearch         = Vue.component( 'search',          Search         );
-const VueProgram        = Vue.component( 'program',         Program        );
-const VueRelease        = Vue.component( 'release',         Release        );
-const VuePreviewProgram = Vue.component( 'preview-program', PreviewProgram );
-const VuePreviewRelease = Vue.component( 'preview-release', PreviewRelease );
-const VueMembers        = Vue.component( 'members',         Members        );
-const VueUpload         = Vue.component( 'upload',          Upload         );
-const VueShop           = Vue.component( 'shop',            Shop           );
-
-// Vuex store
-// const store = new Vuex.Store({
-//   state: {
-//     count: 0
-//   },
-//   mutations: {
-//     increment( state ) {
-//       state.count++;
-//     }
-//   }
-// });
-window.store = store;
-
-const mkApiRequest = ( Type, arg ) => {
-  return ( to, from, next ) => {
-    console.log( 'this is', this );
-    console.log( 'also', { to, from, next });
-    wp.api.loadPromise.done(() => {
-      var object = new Type( arg ? arg( to ): to.params );
-      object.fetch({
-        success: ( model, result, options ) => {
-          console.log( 'got', object, result );
-          if ( result.members_only && ! store.state.user.membership ) {
-            if ( result.preview ) {
-              next({ path: '/preview/' + result.preview });
-            } else
-              next({ path: '/shop/membership', then: to.path });
-          }
-          store.state.nextpost = result;
-          next();
-        },
-        error: ( model, result, options ) => {
-          Vue.swal( "Sorry! We couldn't get you that page.<br/>Please try again later" );
-          next( false );
-        }});
-      // TODO: handle specific errors
-    });
-  };
-};
-
-// Define some routes
-const routes = [
-  { path: '/',                   component: VueHome },
-  { path: '/login/:then?',       component: VueLogin,   name: 'login' },
-  { path: '/about/',             component: VueAbout,   name: 'about' },
-  { path: '/contact/',           component: VueContact, name: 'contact' },
-
-  { path: '/preview/:path/',     component: VuePreviewProgram,
-    name: 'preview-program',
-    beforeEnter: mkApiRequest( wpapix.Preview )},
-
-  { path: '/preview/:program/:release',
-    component: VuePreviewRelease,
-    name: 'preview-release',
-    beforeEnter: mkApiRequest( wpapix.Preview, to => {
-      return { path: to.params.preview + '/' + to.params.release };
-    })},
-
-  { path: '/page/:path',         component: VuePage,    name: 'page',
-    beforeEnter: mkApiRequest( wpapix.Path )},
-  { path: '/post/:path',         component: VuePost,    name: 'post',
-    beforeEnter: mkApiRequest( wpapix.Path )},
-  { path: '/category/:category', component: VueArchive, name: 'category', props: true },
-  { path: '/tag/:tag',           component: VueArchive, name: 'tag', props: true },
-  { path: '/blog/',              component: VueArchive, name: 'blog' },
-  { path: '/search/',            component: VueSearch,  name: 'search' },
-  { path: '/shop/',              component: VueShop,    name: 'shop' },
-  { path: '/members/',           component: VueMembers, name: 'members',
-    meta: { auth: true }},
-  { path: '/upload/',            component: VueUpload,  name: 'upload',
-    meta: { auth: true }},
-  { path: '/:program/:release',  component: VueRelease, name: 'release',
-    beforeEnter: mkApiRequest( wpapix.Release )},
-  { path: '/:path',              component: VueSingle,  name: 'single',
-    beforeEnter: mkApiRequest( wpapix.Path )},
-  { path: "*",                   component: VuePageNotFound }
-];
-
-// Create the router instance and pass the `routes` option
-const router = new VueRouter({
-  routes: routes, 
-  mode: 'history'
-});
-
-// global guard, manage redirection based on authentication and payment
-// fetch post first so we can decide some more specific routing
-router.beforeEach(( to, from, next )  => {
-  if ( to.meta.auth && ! store.state.user ) {
-    next({ path: '/login' + to.path });
-  } else {
-    next();
-  }
-});
-
-// Create and mount the root instance.
-const app = new Vue({
-  router : router, 
-  data : {
-    "site" : {
-      "title" : "", 
-      "url" : "",
-      "description" : ""
-    },
-    sstate: store.state,
-    user: {},
-    menu: []
-  },
-  mounted() { 
-    this.getSiteInfo();
-    this.getUserData();
-    this.getMenu();
-  },    
-  watch : {
-  }, 
-  methods : {
-    getSiteInfo() {
-      this.site.title = this.sstate.site.title || "My Blog";
-      this.site.url = this.sstate.site.url; // TODO: or?
-      this.site.description = this.sstate.site.description;
-    },
-    getUserData() {
-    },
-    getMenu() {
-      this.menu = this.sstate.menus.nav || [];
-    }
-  }
-}).$mount('#app');
-
-// Now the app has started!
-
+/***/ })
+/******/ ]);
+//# sourceMappingURL=main.js.map
