c4b1c3e2dc4a74d985d83e4d5f296525bd7df988
Meshach Mitchell <rain@mawu.home>
2018-12-26 17:24:47 -0500

    php

--- functions.php
+++ functions.php
@@ -985,7 +985,7 @@ add_action( 'wp_enqueue_scripts',            'mrk_enqueue_styles',          999 
 add_action( 'widgets_init',                  'mrk_widgets_init'                    );
 add_filter( 'excerpt_length',                'mrk_excerpt_length',          999    );
 add_filter( 'rest_allow_anonymous_comments', 'allow_anonymous_comments'            );
-add_filter( 'rest_js_client_settings',       'mrk_rest_js_client_settings',        );
+add_filter( 'rest_js_client_settings',       'mrk_rest_js_client_settings'         );
 add_filter( 'mrk_rest_process_post',         'mrk_rest_add_bg_image',        10, 1 );
 add_filter( 'mrk_rest_process_post',         'mrk_rest_add_rel_path',        10, 1 );
 add_filter( 'mrk_rest_process_post',         'mrk_rest_restrictions',       999, 1 );
