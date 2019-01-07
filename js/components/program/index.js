a1d922445a91b2c674b9b6cfecbcca5e4eea0b52
Meshach Mitchell <rain@mawu.home>
2019-01-07 00:10:24 -0500

    actually get member page posttdata

--- js/components/program/index.js
+++ js/components/program/index.js
@@ -1,4 +1,5 @@
 import store from '../../lib/store';
+import _wpapix from '../../lib/wpapix';
 import he from 'he';
 export default {
   template: require( './template.html' ),
@@ -12,11 +13,19 @@ export default {
     };
   },
   mounted() {
-    this.storedPost = Object.assign( {}, this.sstate.nextpost );
-    document.title = he.decode( this.title + ' | ' + this.sstate.site.title );
-    window.setTimeout(() => {
-      this.promos = this.promo_reel;
-    }, 3000);
+    _wpapix.then( wpapix => {
+      var path = new wpapix.Path({ path: '/members' });
+      console.log( 'path object', path );
+      path.fetch().done(( rpost ) => {
+        console.log( 'got members page', rpost );
+        this.storedPost = rpost;
+        document.title = he.decode( rpost.title.rendered + ' | '
+                                    + this.sstate.site.title );
+        window.setTimeout(() => {
+          this.promos = rpost.promo_reel || [];
+        }, 3000 );
+      });
+    });
   },
   updated() {
     document.title = he.decode( this.title + ' | ' + this.sstate.site.title );
