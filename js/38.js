df76af430da1e7bf2740b6310855a60276b541d0
Meshach Mitchell <rain@mawu.home>
2019-01-18 16:35:09 -0500

    try move call-to-action to action bar

--- js/38.js
+++ js/38.js
@@ -45,7 +45,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<header id=\"masthead\" class=\"header clear\">\n  <div id=\"banner-notices\"></div>\n  <div class=\"action-bar\">\n    <user-block></user-block>\n    <search-form></search-form>\n  </div>\n  <div class=\"logo\">\n    <router-link to=\"/\"><img :src=\"logo\" class=\"logo-img\"/></router-link>\n  </div>\n  <h1 class=\"site-title\"><router-link to=\"/\">{{ title }}</router-link></h1>\n  <p class=\"site-description\">{{ description }}</p>\n</header>\n";
+module.exports = "<header id=\"masthead\" class=\"header clear\">\n  <div id=\"banner-notices\"></div>\n  <div class=\"action-bar\">\n    <b-btn v-if=\"!isSubscriber\" class=\"member-cta\" variant=\"warning\"\n           to=\"/members\">Become A VIP Today!</b-btn>\n    <user-block></user-block>\n    <search-form></search-form>\n  </div>\n  <div class=\"logo\">\n    <router-link to=\"/\"><img :src=\"logo\" class=\"logo-img\"/></router-link>\n  </div>\n  <h1 class=\"site-title\"><router-link to=\"/\">{{ title }}</router-link></h1>\n  <p class=\"site-description\">{{ description }}</p>\n</header>\n";
 
 /***/ })
 
