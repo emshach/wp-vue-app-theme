287eb74901cf9f953d069e00030131553ead5edb
Ponder Stibbons <theponderstibbons@gmail.com>
2019-02-08 12:04:58 -0500

    tweak email template

--- templates/email/partials/header.php
+++ templates/email/partials/header.php
@@ -18,21 +18,21 @@ $wrapper = "
 	padding: 70px 0 70px 0;
 ";
 $border_radius = $settings['template'] == 'boxed' ? '6px' : '0px';
+$border_top_radius = $settings['template'] == 'boxed' ? '2px' : '0px';
 $template_container = "
-	-webkit-box-shadow:0 0 0 3px rgba(0,0,0,0.025) !important;
-	box-shadow:0 0 0 3px rgba(0,0,0,0.025) !important;
+	-webkit-box-shadow:0 2px 8px rgba(0,0,0,0.5) !important;
+	box-shadow:0 2px 8px rgba(0,0,0,0.5) !important;
 	-webkit-border-radius:$border_radius !important;
 	border-radius:$border_radius !important;
 	background-color: #fafafa;
-	border-radius:6px !important;
 	max-width: ". ($settings['template'] == 'boxed' ? $settings['body_size'].'px' : '100%') .";";
 $template_header = "
 	background-color: ".$settings['header_bg'].";
 	color: #f1f1f1;
-	-webkit-border-top-left-radius:$border_radius !important;
-	-webkit-border-top-right-radius:$border_radius !important;
-	border-top-left-radius:$border_radius !important;
-	border-top-right-radius:$border_radius !important;
+	-webkit-border-top-left-radius:$border_top_radius !important;
+	-webkit-border-top-right-radius:$border_top_radius !important;
+	border-top-left-radius:$border_top_radius !important;
+	border-top-right-radius:$border_top_radius !important;
 	border-bottom: 0;
 	font-family:Arial;
 	font-weight:bold;
@@ -64,6 +64,13 @@ $header_content_h1_a = "
 	color: ".$settings['header_text_color'].";
 	text-decoration: none;
 ";
+$header_content_h1_img = "
+	-webkit-border-top-left-radius:$border_top_radius !important;
+	-webkit-border-top-right-radius:$border_top_radius !important;
+	border-top-left-radius:$border_top_radius !important;
+	border-top-right-radius:$border_top_radius !important;
+        max-width: 100%;
+";
 ?>
 <html>
   <head>
@@ -94,7 +101,7 @@ $header_content_h1_a = "
                         <h1 style="<?php echo $header_content_h1; ?>" id="logo">
 		          <a style="<?php echo $header_content_h1_a;?>" href="<?php echo apply_filters( 'mailtpl/templates/header_logo_url', home_url());?>" title="<?php echo apply_filters( 'mailtpl/templates/header_logo_url_title', !empty($settings['header_logo_text']) ? do_shortcode( strip_tags($settings['header_logo_text']) ) : get_bloginfo('name') );?>"><?php
 		                                            if( !empty($settings['header_logo']) ) {
-			                                            echo '<img style="max-width:100%;" src="'.apply_filters( 'mailtpl/templates/header_logo', $settings['header_logo'] ).'" alt="'. apply_filters( 'mailtpl/templates/header_logo_alt', !empty($settings['header_logo_text']) ? do_shortcode( strip_tags($settings['header_logo_text']) ) : get_bloginfo( 'description' ) ) .'"/>';
+			                                            echo '<img style="'.$header_content_h1_img.'" src="'.apply_filters( 'mailtpl/templates/header_logo', $settings['header_logo'] ).'" alt="'. apply_filters( 'mailtpl/templates/header_logo_alt', !empty($settings['header_logo_text']) ? do_shortcode( strip_tags($settings['header_logo_text']) ) : get_bloginfo( 'description' ) ) .'"/>';
 		                                            } elseif ( !empty( $settings['header_logo_text'] ) ) {
 														echo do_shortcode($settings['header_logo_text']);
 		                                            } else {
